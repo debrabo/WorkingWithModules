d4c58d7d3c2290e48f5cec1431856931338af9f1
eberkel <mwm.vanberkel@studie.ou.nl>
2020-12-14 11:02:46 +0100

    Set v0_8b Named Imports

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,13 @@
-import getSessions from "./sessionRepository.js"; // is default so you can name it for example 'john'
-import {sessionTemplate, errorMessage} from "./template.js";
+import getSessions, {sessionTemplate, sessionURL} from "./sessionRepository.js"; // is default so you can name it for example 'john'
+// import getSessions, * as sessionRepository from './template.js';
+// import {sessionTemplate, errorMessage} from "./template.js";
 // import * as template from "./template.js";
 
 function render() {
     var list = document.querySelector('#sessions');
     if (!list) return;
     list.innerHTML = sessionTemplate(data.listItems);
+    // list.innerHTML = sessionRepository.sessionTemplate(data.listItems);
     // list.innerHTML = template.sessionTemplate(data.listItems);
 };
 
